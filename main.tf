24bc2c585adc31604f68a8a5bddae77107747f66
AKhilesh <kothapallyakhil123@gmail.com>
2023-11-22 15:10:50 +0530

    updated code

--- main.tf
+++ main.tf
@@ -4,3 +4,13 @@ resource "aws_docdb_subnet_group" "main" {
   tags = merge(local.tags, { Name = "${local.name_prefix}-subnet-group"})
 
 }
+
+resource "aws_docdb_cluster" "main" {
+  cluster_identifier      = "${local.name_prefix}-cluster"
+  engine                  = "docdb"
+  master_username         = "foo"
+  master_password         = "mustbeeightchars"
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true
+}
