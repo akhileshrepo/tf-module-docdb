5f807b8cc801b98d012aa222161eef5707247b70
AKhilesh <kothapallyakhil123@gmail.com>
2023-11-22 19:28:51 +0530

    updated code

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "aws_docdb_cluster" "main" {
   preferred_backup_window = var.preferred_backup_window
   skip_final_snapshot     = var.skip_final_snapshot
   db_subnet_group_name    = aws_docdb_subnet_group.main.name
-  vpc_security_group_ids = aws_security_group.main.id
+  vpc_security_group_ids = [aws_security_group.main.id]
   db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.main.name
   tags = merge(local.tags, { Name = "${local.name_prefix}-cluster"})
   engine_version = var.engine_version
