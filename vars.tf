2331e1680d75b1b303a6acb3676342037b3fef06
AKhilesh <kothapallyakhil123@gmail.com>
2023-11-25 11:25:09 +0530

    updated code

--- vars.tf
+++ vars.tf
@@ -5,3 +5,11 @@ variable "tags" {}
 
 variable "vpc_id" {}
 variable "component" {}
+variable "engine_family" {}
+variable "sg_ingress_cidr" {}
+variable "backup_retention_period" {}
+variable "preferred_backup_window" {}
+variable "skip_final_snapshot" {}
+variable "engine_version" {}
+variable "instance_count" {}
+variable "instance_class" {}
