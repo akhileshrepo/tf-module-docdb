ce6893e01b9a7a3cefc6072bd0c900fb7bc041a6
AKhilesh <kothapallyakhil123@gmail.com>
2023-11-26 00:05:53 +0530

    updated code

--- main.tf
+++ main.tf
@@ -1,17 +1,18 @@
 resource "aws_docdb_subnet_group" "main" {
   name       = "${local.name_prefix}-subnet-group"
   subnet_ids = var.subnet_ids
-  tags = merge(local.tags, { Name = "${local.name_prefix}-subnet-group"})
-
+  tags       = merge(local.tags, { Name = "${local.name_prefix}-subnet-group" })
 }
+
+
 resource "aws_security_group" "main" {
   name        = "${local.name_prefix}-sg"
   description = "${local.name_prefix}-sg"
   vpc_id      = var.vpc_id
-  tags = merge(local.tags, {Name = "${local.name_prefix}-sg"})
+  tags        = merge(local.tags, { Name = "${local.name_prefix}-sg" })
 
   ingress {
-    description = var.engine_family
+    description = "DOCDB"
     from_port   = 27017
     to_port     = 27017
     protocol    = "tcp"
@@ -27,31 +28,35 @@ resource "aws_security_group" "main" {
   }
 }
 
+
 resource "aws_docdb_cluster_parameter_group" "main" {
-  family      = "docdb4.0"
+  family      = var.engine_family
   name        = "${local.name_prefix}-pg"
   description = "${local.name_prefix}-pg"
+  tags        = merge(local.tags, { Name = "${local.name_prefix}-pg" })
 
 }
 
 resource "aws_docdb_cluster" "main" {
-  cluster_identifier      = "${local.name_prefix}-cluster"
-  engine                  = "docdb"
-  master_username         = data.aws_ssm_parameter.master_username.value
-  master_password         = data.aws_ssm_parameter.master_password.value
-  backup_retention_period = var.backup_retention_period
-  preferred_backup_window = var.preferred_backup_window
-  skip_final_snapshot     = var.skip_final_snapshot
-  db_subnet_group_name    = aws_docdb_subnet_group.main.name
-  vpc_security_group_ids = [aws_security_group.main.id]
+  cluster_identifier              = "${local.name_prefix}-cluster"
+  engine                          = "docdb"
+  master_username                 = data.aws_ssm_parameter.master_username.value
+  master_password                 = data.aws_ssm_parameter.master_password.value
+  backup_retention_period         = var.backup_retention_period
+  preferred_backup_window         = var.preferred_backup_window
+  skip_final_snapshot             = var.skip_final_snapshot
+  db_subnet_group_name            = aws_docdb_subnet_group.main.name
+  vpc_security_group_ids          = [aws_security_group.main.id]
   db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.main.name
-  tags = merge(local.tags, { Name = "${local.name_prefix}-cluster"})
-  engine_version = var.engine_version
+  tags                            = merge(local.tags, { Name = "${local.name_prefix}-cluster" })
+  engine_version                  = var.engine_version
+  storage_encrypted               = true
 }
+
 
 resource "aws_docdb_cluster_instance" "main" {
   count              = var.instance_count
-  identifier         = "${local.name_prefix}-cluster-instance-${count.index+1}"
+  identifier         = "${local.name_prefix}-cluster-instance-${count.index + 1}"
   cluster_identifier = aws_docdb_cluster.main.id
   instance_class     = var.instance_class
 }
