51661f9ca067f8d2b6eecaf3933c32892682c2c6
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-12 13:55:01 +0530

    updated code

--- vars.tf
+++ vars.tf
@@ -1,3 +1,8 @@
 variable "env" {}
 variable "subnet_ids" {}
 variable "tags" {}
+variable "backup_retention_period" {}
+variable "preferred_backup_window" {}
+variable "skip_final_snapshot" {}
+variable "vpc_id" {}
+variable "sg_ingress_cidr" {}
