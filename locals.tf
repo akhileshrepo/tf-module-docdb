715f4c54746f82811cd9a248b898f4b1a263eea0
AKhilesh <kothapallyakhil123@gmail.com>
2024-03-22 10:55:02 +0530

    updated code

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
+  tags    = merge(var.tags, { tf-module-name = "docdb" }, { env = var.env })
   name_prefix = "${var.env}-docdb"
-  tags        = merge(var.tags, { tf-module-name = "docdb" }, { env = var.env })
 }
