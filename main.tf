c12d3ee322e652e4d0ab8aae1afe0cbdaba70668
AKhilesh <kothapallyakhil123@gmail.com>
2024-03-23 11:06:25 +0530

    updated code

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "aws_security_group" "main" {
 }
 
 resource "aws_docdb_cluster_parameter_group" "main" {
-  family      = "docdb4.0"
+  family      = var.engine_family
   name        = "${local.name_prefix}-pg"
   description = "${local.name_prefix}-pg"
   tags        = merge(local.tags, { Name = "${local.name_prefix}-pg" })
--- vars.tf
+++ vars.tf
@@ -7,3 +7,4 @@ variable "skip_final_snapshot" {}
 variable "vpc_id" {}
 variable "sg_ingress_cidr" {}
 variable "engine_version" {}
+variable "engine_family" {}
