a0f351f7cfa689a7e5d0caae77276cc1afb9f5c0
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-30 18:51:05 +0530

    updated code

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_docdb_cluster" "main" {
   engine                  = "docdb"
   master_username         = data.aws_ssm_parameter.master_username.value
   master_password         = data.aws_ssm_parameter.master_password.value
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
-  skip_final_snapshot     = true
+  backup_retention_period = var.backup_retention_period
+  preferred_backup_window = var.preferred_backup_window
+  skip_final_snapshot     = var.skip_final_snapshot
 }
--- vars.tf
+++ vars.tf
@@ -1,3 +1,6 @@
 variable "env" {}
 variable "subnet_ids" {}
 variable "tags" {}
+variable "skip_final_snapshot" {}
+variable "preferred_backup_window" {}
+variable "backup_retention_period" {}
